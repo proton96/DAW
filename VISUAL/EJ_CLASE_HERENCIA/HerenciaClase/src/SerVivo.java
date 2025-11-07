cc892d9b93ff1768be25965856754c6014a38e47
PROTON <josecarlosmartinlopez10@gmail.com>
2025-11-07 16:35:18 +0100

    EJERCICIO CLASE HERENCIA

---
+++ VISUAL/EJ_CLASE_HERENCIA/HerenciaClase/src/SerVivo.java
@@ -0,0 +1,4 @@
+public abstract class SerVivo {
+    // Método abstracto que debe ser implementado por las subclases
+    public abstract void alimentarse();
+}
---
+++ VISUAL/EJ_CLASE_HERENCIA/HerenciaClase/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) throws Exception {
+        SerVivo planta = new Planta();
+        SerVivo humano = new Humano();
+
+        planta.alimentarse();
+        humano.alimentarse();
+    }
+}
